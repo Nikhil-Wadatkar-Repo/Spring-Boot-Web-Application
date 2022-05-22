fb462e28967caff612382543a58bbd903f3c9ea7
Ankur <ankur.wadatkar@gmail.com>
2022-05-22 12:02:38 +0530

    remove unused code

--- src/main/java/com/bs/CustomeActuator.java
+++ src/main/java/com/bs/CustomeActuator.java
@@ -3,12 +3,8 @@ package com.bs;
 public class CustomeActuator {
   public CustomeActuator (){
 
-<<<<<<< HEAD
-  }
-=======
-  public CustomeActuator () {
 
   }
 
->>>>>>> 56265c7 (branch 01 changes)
+
 }
